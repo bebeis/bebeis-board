bec37483e60599a3437693f24d415ed68182b86d
Junseo Bae <ferrater1013@gmail.com>
2025-03-30 01:08:29 +0900

    test(article): article CRUD Api 테스트 추가

--- service/article/src/main/java/bebeis/board/article/controller/ArticleController.java
+++ service/article/src/main/java/bebeis/board/article/controller/ArticleController.java
@@ -5,6 +5,7 @@ import bebeis.board.article.service.request.ArticleCreateRequest;
 import bebeis.board.article.service.request.ArticleUpdateRequest;
 import bebeis.board.article.service.response.ArticleResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ArticleController {
 
     private final ArticleService articleService;
 
     @GetMapping("v1/articles/{articleId}")
     public ArticleResponse read(@PathVariable Long articleId) {
+        log.info("{} read request", articleId);
         return articleService.read(articleId);
     }
 
@@ -37,5 +40,6 @@ public class ArticleController {
     @DeleteMapping("/v1/articles/{articleId}")
     public void delete(@PathVariable Long articleId) {
         articleService.delete(articleId);
+        log.info("{} article delete", articleId);
     }
 }
---
+++ service/article/src/test/java/bebeis/board/article/api/ArticleApiTest.java
@@ -0,0 +1,66 @@
+package bebeis.board.article.api;
+
+import bebeis.board.article.service.request.ArticleCreateRequest;
+import bebeis.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest() {
+        ArticleResponse response = create(new ArticleCreateRequest("hi", "my content", 1L, 1L));
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void readTest() {
+        ArticleResponse response = read(164401238888845312L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse read(Long articleId) {
+        return restClient.get().uri("v1/articles/{articleId}", articleId).retrieve().body(ArticleResponse.class);
+    }
+
+    ArticleResponse create(ArticleCreateRequest request) {
+        return restClient.post().uri("/v1/articles").body(request).retrieve().body(ArticleResponse.class);
+    }
+
+    @Test
+    void updateTest() {
+        update(164401238888845312L);
+        ArticleResponse response = read(164401238888845312L);
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void deleteTest() {
+        restClient.delete().uri("/v1/articles/{articleId}", 164401238888845312L).retrieve().toBodilessEntity();
+    }
+
+    void update(Long articleId) {
+        restClient.put().uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("hi 2", "my content 2")).retrieve().body(ArticleResponse.class);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest {
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+        private String title;
+        private String content;
+    }
+}
