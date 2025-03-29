77ef6e91446b219bf8c7b1f3a424ad014d57cec6
Junseo Bae <ferrater1013@gmail.com>
2025-03-30 01:07:10 +0900

    fix(article): 애노테이션 누락 수정

--- service/article/src/main/java/bebeis/board/article/repository/ArticleRepository.java
+++ service/article/src/main/java/bebeis/board/article/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package bebeis.board.article.repository;
 
 import bebeis.board.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
